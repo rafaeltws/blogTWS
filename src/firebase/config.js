c45717f9afedadfe11fb1e59eed9a74e520897b3
rafaeltws <159142263+rafaeltws@users.noreply.github.com>
2024-12-06 00:06:14 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_LQG6NCAnhCbh_0NUaF3uV7C86H8VA_c",
-  authDomain: "miniblog-80af3.firebaseapp.com",
-  projectId: "miniblog-80af3",
-  storageBucket: "miniblog-80af3.firebasestorage.app",
-  messagingSenderId: "226852213144",
-  appId: "1:226852213144:web:88bd52aa2ae7c9d4c55bed"
+  apiKey: //,
+  authDomain: //,
+  projectId: //,
+  storageBucket: //,
+  messagingSenderId: //,
+  appId: //
 };
 
 const app = initializeApp(firebaseConfig);
